eee3e90d164be58c2d5e2b8f141bc0a1b7e5d1cb
Suhhanii <suhanikaushal57@gmail.com>
2025-10-01 14:34:20 +0530

    Initial Commit

---
+++ src/main/java/com/turfNest/TurfNesttApplication.java
@@ -0,0 +1,13 @@
+package com.turfNest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurfNesttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurfNesttApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/turfNest/TurfNesttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turfNest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurfNesttApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
